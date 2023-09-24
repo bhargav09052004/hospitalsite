90bbb81db287e9f561715c70183556ddb4d1d17d
bhargav09052004 <109296619+bhargav09052004@users.noreply.github.com>
2023-09-24 18:32:46 +0530

    sign in

--- app.py
+++ app.py
@@ -1,22 +1,18 @@
-# from flask import Flask, render_template, json, request, session, redirect
-# from flaskext.mysql import MySQL
-
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
-# pip install flask_mysqldb
-import MySQLdb.cursors as mdc
-# pip install flask-MySQLdb
-
-import re
 
 mysql = MySQL()
 app = Flask(__name__)
 
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'Bhargav@2004'
-app.config['MYSQL_DATABASE_DB'] = 'appointment'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'ash789#p'
+app.config['MYSQL_DATABASE_DB'] = 'healthcare'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
+
+patient_counter=0
+appoint_counter=0
+reschedule_counter=0
 
 @app.route('/doctor-availability')
 def availability():
@@ -38,108 +34,135 @@ def view_schedule():
 def patient_gateway():
     return render_template('patient-gateway.html')
 
+@app.route('/hospital')
+def main():
+    return render_template('hospital.html')
+
 @app.route('/sign-up')
-def sign_up():
+def signup():
     return render_template('sign-up.html')
 
 @app.route('/sign-in')
-def sign_in():
+def signin():
     return render_template('sign-in.html')
 
-@app.route('/hospital')
-def hospital():
-    return render_template('hospital.html')
-
 @app.route('/')
-def main():
+def intro():
     return render_template('start.html')
 
-@app.route('/booking-schedule',methods=['POST'])
-def Appointment_Booking():
-  msg = ''
-  if request.method == 'POST' and 'firstname' in request.form and 'lastname' in request.form and 'phonenumber' in request.form and 'symptoms' in request.form and 'dob' in request.form and 'departmentcode' in request.form and 'postalcode' in request.form and 'specific_requirements' in request.form and  'occupation' in request.form:
+@app.route('/sign-up',methods=['POST'])
+def signUp():
+   msg=''
+   global patient_counter
+   if request.method == 'POST':
     firstname = request.form['firstname']
     lastname = request.form['lastname']
     phonenumber = request.form['phonenumber']
     email = request.form['email']
     dob = request.form['dob']
-    departmentcode = request.form['departmentcode']
-    symptoms = request.form['symptoms']
-    specific_requirements = request.form['specific_requirements']
-    occupation = request.form['occupation']
-
+    password = request.form['password']
+    current_password=request.form['current_password']
+    
     conn = mysql.connect()
     cursor = conn.cursor()
-    cursor = mysql.connection.cursor()
-    #mdc.DictCursor
-    # cursor.execute(
-    #     'SELECT * FROM Appointment WHERE phonenumber = % s', (phonenumber, ))
-    # account = cursor.fetchone()
-    # if account:
-    #     msg = 'Appointment already Taken !'
-    # if not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-    #     msg = 'Invalid email address !'
-    # elif not re.match(r'[A-Za-z]+', firstname):
-    #     msg = 'name must contain only characters!'
-    # elif not re.match(r'[A-Za-z]+', lastname):
-    #     msg = 'name must contain only characters!'  
-#  else:
-    cursor.execute("INSERT INTO appointment (firstname, lastname, phonenumber,email,dob, departmentcode, symptoms,specific_requirements, occupation) VALUES (% s, % s, % d, % s, % s, % s, % s, % s, % s)",
-                    (firstname, lastname, phonenumber,email,
-                    dob, departmentcode, symptoms,
-                    specific_requirements, occupation))
-    mysql.connection.commit()
-    msg = 'You Appoinment is successfully scheduled!'
-  return render_template('hospital.html',msg=msg)
-
-# redirect(url_for('hospital'))
-
-# @app.route('/Appointment/Reschedule', methods=['GET', 'POST'])
-# def Appointment_Rescheduler():
-#   msg = ''
-#   if request.method == 'POST' and 'firstname' in
-#   request.form and 'lastname' in request.form and
-#   'phonenumber' in request.form and 'email2' in
-#   request.form and 'dob' in request.form and
-#   'pdt' in request.form and 'pdt2'
-#   in request.form:
-#     firstname = request.form['firstname']
-#     lastname = request.form['lastname']
-#     phonenumber = request.form['phonenumber']
-#     email = request.form['email']
-#     email2 = request.form['email2']
-#     dob = request.form['dob']
-#     pdt = request.form['pdt']
-#     pdt2 = request.form['pdt2']
-#     # specific_requirements = request.form['specific_requirements']
-#     # occupation = request.form['occupation']
-
-#     conn = mysql.connect()
-#     cursor = conn.cursor()
-#     # cursor = mysql.connection.cursor(mdc.DictCursor)
-#     # cursor.execute(
-#     #     'SELECT * FROM Appointment WHERE phonenumber = % s', (phonenumber, ))
-#     # account = cursor.fetchone()
-#     # if account:
-#     #     msg = 'Appointment already Taken !'
-#     if not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-#         msg = 'Invalid email address !'
-#     elif not re.match(r'[^@]+@[^@]+\.[^@]+', email2):
-#         msg = 'Invalid email address !'    
-#     elif not re.match(r'[A-Za-z]+', firstname):
-#         msg = 'name must contain only characters!'
-#     elif not re.match(r'[A-Za-z]+', lastname):
-#         msg = 'name must contain only characters!'  
-#     else:
-#         cursor.execute('INSERT INTO Appointment_Rescheduler VALUES \
-#         (NULL, % s, % s, % s, % s, % s, % s, % s, % s, % s)',
-#                         (firstname, lastname, phonenumber,email,
-#                         email2, dob, pdt, pdt2,))
-#         mysql.connection.commit()
-#         msg = 'You Appoinment is successfully Re-scheduled! to {{pdt2}}'
-#   return render_template('register.html', msg=msg)
-
-
-
+    if(current_password==password):
+        patient_counter=patient_counter+1
+        patient_id="PH"+lastname[0]+firstname[0]+str(patient_counter)
+        cursor.execute("INSERT INTO signup (patient_id,firstname, lastname,email, phonenumber,dob,password) VALUES (% s, % s, % s, % s, % s, %s, %s)",
+                    (patient_id,firstname, lastname,email, phonenumber,dob,password,))
+        conn.commit()
+        msg='You have successfully registered your self!'
+        return render_template('hospital.html',msg=msg)
+    else:
+        msg='Enter correct password values!!!'
+        return render_template('sign-up.html',msg=msg)
+    
+@app.route('/booking-schedule',methods=['POST'])
+def appoint():
+   msg=''
+#    print(request.form.keys)
+#    print(request.form.values)
+   global appoint_counter
+   if request.method == 'POST':
+    patient_id = request.form['patient_id']
+    # lastname = request.form['lastname']
+    # phonenumber = request.form['phonenumber']
+    department = request.form['department']
+    doctorname= request.form['availabledoctor']
+    symptom = request.form['symptom']
+    category = request.form['category']
+    date = request.form['date']
+    time = request.form['time']
+    
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    
+    cursor.execute("Select patient_id from signup where patient_id=%s",(patient_id,))
+    a=cursor.fetchone()[0]
+    if a:
+        appoint_counter=appoint_counter+1
+        appointment_id="AH"+a[0]+str(appoint_counter)
+        cursor.execute("INSERT INTO appointment (patient_id,appointment_id, category,date,time,department, symptom,doctorname) VALUES (% s, % s, % s, % s, % s, %s, %s, %s)",
+                    (patient_id,appointment_id, category,date,time,department, symptom,doctorname,))
+        conn.commit()
+        msg='You Appointment is scheduled successfully at '+date+' '+time+' '+doctorname
+        return render_template('patient-gateway.html',msg=msg)
+    else:
+        msg='Invalid Appointment Id!!!'
+        return render_template('booking-schedule.html',msg=msg)
+    return render_template('booking-schedule.html')    
+    
+@app.route('/reschedule',methods=['POST'])
+def rescheduler():
+   msg=''
+#    print(request.form.keys)
+#    print(request.form.values)
+   global reschedule_counter
+   if request.method == 'POST':
+    appointment_id = request.form['appointment_id']
+    # lastname = request.form['lastname']
+    # phonenumber = request.form['phonenumber']
+    changevalue = request.form['changevalue']
+    date = request.form['cad']
+    time = request.form['cat']
+    # print(changevalue)
+    # return render_template('reschedule.html',msg=msg) 
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    if(changevalue=='no'):
+        cursor.execute("Select patient_id,category,department,symptom,doctorname from appointment where appointment_id=%s",(appointment_id,))
+        a=cursor.fetchone()
+        if a:
+            reschedule_counter=reschedule_counter+1
+            appoint_id="RH"+a[0][0]+str(reschedule_counter)
+            cursor.execute("Delete from appointment where appointment_id=%s",(appointment_id,))
+            conn.commit()
+            cursor.execute("INSERT INTO appointment (patient_id,appointment_id, category,date,time,department, symptom,doctorname) VALUES (% s, % s, % s, % s, % s, %s, %s, %s)",
+                        (a[0],appoint_id, a[1],date,time,a[2], a[3],a[4],))
+            conn.commit()
+            msg='You Appointment is successfully re-scheduled to '+date+' '+time+' '+a[4]
+            return render_template('patient-gateway.html',msg=msg)
+        else:
+            msg='Invalid Appointment Id!!!'
+            return render_template('reschedule.html',msg=msg)
+    else:
+        cursor.execute("Select patient_id,category,department,symptom from appointment where appointment_id=%s",(appointment_id,))
+        a=cursor.fetchone()
+        if a:
+            changedoctor= request.form['changedoctor']
+            reschedule_counter=reschedule_counter+1
+            appoint_id="AH"+a[0][0]+str(reschedule_counter)
+            cursor.execute("Delete from appointment where appointment_id=%s",(appointment_id,))
+            conn.commit()
+            cursor.execute("INSERT INTO appointment (patient_id,appointment_id, category,date,time,department, symptom,doctorname) VALUES (% s, % s, % s, % s, % s, %s, %s, %s)",
+                        (a[0],appoint_id, a[1],date,time,a[2], a[3],changedoctor,))
+            conn.commit()
+            msg='You Appointment is successfully re-scheduled to '+date+' '+time+' '+changedoctor
+            return render_template('patient-gateway.html',msg=msg)
+        else:
+            msg='Invalid Appointment Id!!!'
+            return render_template('reschedule.html',msg=msg)  
+    
+    
 if __name__ == "__main__":
     app.run(debug=True)
